2a892a161cadc947111babba6b7c230871508590
Michael Klein <michael@firstiwaslike.com>
2023-12-01 20:27:20 +0100

    Add chatgpt, obsidian, use tokyonight theme

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,18 @@
+-- Use obsidian from within neovim
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = os.getenv("OBSIDIAN_VAULT"),
+      },
+    },
+  },
+}
