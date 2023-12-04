6dd98404bb49a1798630f52770de4541fa74b6a8
Michael Klein <michael@firstiwaslike.com>
2023-12-04 14:00:30 +0100

    Configure eslint properly for gts/gjs files

--- lua/plugins/glimmer.lua
+++ lua/plugins/glimmer.lua
@@ -1,8 +1,32 @@
 -- install glint language server
 return {
-  "williamboman/mason.nvim",
-  opts = function(_, opts)
-    opts.ensure_installed = opts.ensure_installed or {}
-    table.insert(opts.ensure_installed, "glint")
-  end,
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "glint")
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        eslint = {
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+            "vue",
+            "svelte",
+            "astro",
+            "typescript.glimmer",
+            "javascript.glimmer",
+          },
+        },
+      },
+    },
+  },
 }
