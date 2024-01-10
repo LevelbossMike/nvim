93efbd42d7ff57e4d8b15cd9c4cbd67f2a16b6df
Michael Klein <michael@firstiwaslike.com>
2024-01-10 10:26:30 +0100

    Make the active window more prominent
    To make it more obvious which window is active, we dim the inactive
    ones with a very dark gray.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,13 @@
 return {
+  "folke/tokyonight.nvim",
+  lazy = true,
+  opts = {
+    style = "moon",
+    dim_inactive = true,
+    on_colors = function(colors)
+      colors.bg_dark = "#101010"
+    end,
+  },
   -- "LazyVim/LazyVim",
   -- opts = {
   --   colorscheme = "catppuccin",
